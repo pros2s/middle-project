7904a657e88cc87e649a9f0377e7ff4a67e40ed4
Alexandr <caiiiia_podoplelov@mail.ru>
2022-12-02 11:46:32 +0300

    fetchArticlesNextPage test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -9,7 +9,8 @@ export default {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
   moduleNameMapper: {
-    '\\.s?css$': 'identity-obj-proxy',
+    '\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'JestEmptyComponent'),
   },
   testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
---
+++ src/pages/articlesPage/model/services/fetchArticlesNextPage/fetchArticlesNextPage.test.ts
@@ -0,0 +1,85 @@
+import { ArticleView } from 'entities/article';
+import { TestAsyncThunk } from 'shared/lib/testHelpers/testAsyncThunk';
+import { fetchArticles } from '../fetchArticles/fetchArticles';
+import { articleActions } from '../../slice/ArticleSlice';
+import { fetchArticlesNextPage } from './fetchArticlesNextPage';
+
+jest.mock('../fetchArticles/fetchArticles');
+
+describe('fetchArticlesNextPage.test', () => {
+  test('succes fetch function call', async () => {
+    const thunk = new TestAsyncThunk(fetchArticlesNextPage, {
+      article: {
+        hasMore: true,
+        entities: {},
+        ids: [],
+        isLoading: false,
+        page: 1,
+        view: ArticleView.BIG,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(4);
+    expect(thunk.dispatch).toBeCalledWith(articleActions.setPage(2));
+    expect(thunk.dispatch).toBeCalledWith(fetchArticles({ page: 2 }));
+  });
+
+  test('error fetch with !hasMore and isLoading', async () => {
+    const thunk = new TestAsyncThunk(fetchArticlesNextPage, {
+      article: {
+        hasMore: false,
+        entities: {},
+        ids: [],
+        isLoading: true,
+        page: 1,
+        view: ArticleView.BIG,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(2);
+    expect(thunk.dispatch).not.toBeCalledWith(articleActions.setPage(2));
+    expect(thunk.dispatch).not.toBeCalledWith(fetchArticles({ page: 2 }));
+  });
+
+  test('error fetch with only !hasMore', async () => {
+    const thunk = new TestAsyncThunk(fetchArticlesNextPage, {
+      article: {
+        hasMore: false,
+        entities: {},
+        ids: [],
+        isLoading: false,
+        page: 1,
+        view: ArticleView.BIG,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(2);
+    expect(thunk.dispatch).not.toBeCalledWith(articleActions.setPage(2));
+    expect(thunk.dispatch).not.toBeCalledWith(fetchArticles({ page: 2 }));
+  });
+
+  test('error fetch with only isLoading', async () => {
+    const thunk = new TestAsyncThunk(fetchArticlesNextPage, {
+      article: {
+        hasMore: false,
+        entities: {},
+        ids: [],
+        isLoading: false,
+        page: 1,
+        view: ArticleView.BIG,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(2);
+    expect(thunk.dispatch).not.toBeCalledWith(articleActions.setPage(2));
+    expect(thunk.dispatch).not.toBeCalledWith(fetchArticles({ page: 2 }));
+  });
+});
