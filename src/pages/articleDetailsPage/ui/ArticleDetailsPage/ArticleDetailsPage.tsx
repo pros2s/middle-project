b0f5d3f3a839941181eda5bcd850853210604002
Alexandr Podoplelov <caiiiia_podoplelov@mail.ru>
2023-01-14 04:03:58 +0300

    article details page comment refactor

---
+++ src/pages/articleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,38 @@
+import { FC, memo } from 'react';
+import { useParams } from 'react-router-dom';
+import { ArticleDetails } from 'entities/article';
+
+import { Text, TextAlign, TextThemes } from 'shared/ui/Text/Text';
+
+import { useTranslation } from 'react-i18next';
+import { Page } from 'widgets/Page';
+import { Flex } from 'shared/ui/Stack/Flex';
+import { ArticleDetailsPageComments } from '../ArticleDetailsPageComments/ArticleDetailsPageComments';
+
+const ArticleDetailsPage: FC = () => {
+  const { t } = useTranslation('articlesPage');
+
+  const { id } = useParams<{ id: string }>();
+
+  if (!id && __PROJECT__ !== 'storybook') {
+    return (
+      <Flex direction='column'>
+        <Text
+          title={t('IdNotFound')}
+          align={TextAlign.CENTER}
+          theme={TextThemes.ERROR}
+        />
+      </Flex>
+    );
+  }
+
+  return (
+    <Page>
+      <ArticleDetails id={id} />
+      <Text title={t('comments')} />
+      <ArticleDetailsPageComments id={id} />
+    </Page>
+  );
+};
+
+export default memo(ArticleDetailsPage);
---
+++ src/pages/articleDetailsPage/ui/ArticleDetailsPageComments/ArticleDetailsPageComments.tsx
@@ -0,0 +1,51 @@
+import { memo, useCallback } from 'react';
+import { CommentList } from 'entities/comment';
+import { useSelector } from 'react-redux';
+import {
+  ReducersList,
+  DynamicReducerLoader,
+} from 'shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
+import { useFetchEffect } from 'shared/lib/hooks/useFetchEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
+import { AddComment } from 'features/addComment';
+import { getIsLoadingFromComments } from '../../model/selectors/getFromComments';
+import {
+  articleDetailsPageReducer,
+  getAllComments,
+} from '../../model/slice/ArticleDetailsPageSlice';
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
+
+interface ArticleDetailsPageCommentsProps {
+  id?: string;
+}
+
+const reducers: ReducersList = {
+  articleComments: articleDetailsPageReducer,
+};
+
+export const ArticleDetailsPageComments = memo(
+  ({ id }: ArticleDetailsPageCommentsProps) => {
+    const dispatch = useAppDispatch();
+    const comments = useSelector(getAllComments.selectAll);
+    const isLoading = useSelector(getIsLoadingFromComments);
+
+    useFetchEffect(() => {
+      dispatch(fetchCommentsByArticleId(id));
+    });
+
+    const onSendComment = useCallback(
+      (text: string) => {
+        dispatch(addCommentForArticle(text));
+      },
+      [dispatch],
+    );
+
+    return (
+      <DynamicReducerLoader reducers={reducers}>
+        <AddComment onSendComment={onSendComment} />
+        <CommentList isLoading={isLoading} comments={comments} />
+      </DynamicReducerLoader>
+    );
+  },
+);
